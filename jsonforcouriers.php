6821f3e46b477c110c6b1cac30f733994034c396
ivanm8 <theicelander88@gmail.com>
2017-07-23 20:27:10 +0300

    Update jsonforcouriers.php

--- jsonforcouriers.php
+++ jsonforcouriers.php
@@ -5,24 +5,32 @@
 
 	mb_internal_encoding("UTF-8");
 
-	$mysql_host = "localhost";
-	$mysql_user = "host1585490";
-	$mysql_password = "a5523393";
-	$mysql_dbname = "host1585490";
-	$mysql_tablename = "allDeliveriesFromOnlineStoreEngine";
+	/**
+	* Для того, чтобы доставки, хранящиеся на Вашем сервере, скопировать в формат JSON,
+	* необходимо указать данные от Вашей MySQL-базы данных.
+	* Как Вы можете видеть, эти данные нужны данному скрипту
+	* только для того, чтобы прочитать их.
+	* Данные о Ваших доставках будут доступны только на этой странице.
+	*/
+	$mysql_host = ""; // адрес MySQL-сервера
+	$mysql_user = ""; // имя пользователя для аторизации на Вашем MySQL-сервере
+	$mysql_password = ""; // пароль для пользовтеля, указанного выше
+	$mysql_dbname = ""; // название Вашей MySQL-базы данных, в которой хранится таблица с информацией о доставках
+	$mysql_tablename = ""; // название MySQL-таблицы, в кторой харнится информация о доставках
 
 	$link = mysql_connect($mysql_host, $mysql_user, $mysql_password) 
         or die('Не удалось соединиться: ' . mysql_error());
+
     mysql_select_db($mysql_dbname) or die('Не удалось выбрать базу данных: ' . mysql_error());
     mysql_set_charset('utf8');
 
     $deliveryCourier = $_GET["deliveryCourier"];
     
-    $result = mysql_query("SELECT * FROM allDeliveriesFromOnlineStoreEngine WHERE deliveryStatus = '0' AND deliveryCourier = '".mysql_real_escape_string($deliveryCourier)."'");
-	$rows = array();
-	while($r = mysql_fetch_assoc($result)) {
+    $result = mysql_query("SELECT * FROM $mysql_tablename WHERE deliveryStatus = '0' AND deliveryCourier = '".mysql_real_escape_string($deliveryCourier)."'");
+    $rows = array();
+    while($r = mysql_fetch_assoc($result)) {
     	$rows[] = $r;
-	}
+    }
 
 	header('Content-Type:Application/json; charset=utf-8');
 	echo json_encode($rows, JSON_UNESCAPED_UNICODE);
