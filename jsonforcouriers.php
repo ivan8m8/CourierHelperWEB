5b009b3802d0fba20b2c1dfb7f1e2a4f824fb1be
Ivan <ivan8m8@gmail.com>
2018-01-19 00:58:11 +0300

    Add files via upload
    Убраны переменные с данными для доступа к БД, т.к. ранее они были вынесены в отдельный файл

--- jsonforcouriers.php
+++ jsonforcouriers.php
@@ -5,27 +5,29 @@
 
 	mb_internal_encoding("UTF-8");
 
-	$mysql_host = ""; // адрес MySQL-сервера
-	$mysql_user = ""; // имя пользователя для аторизации на Вашем MySQL-сервере
-	$mysql_password = ""; // пароль для пользовтеля, указанного выше
-	$mysql_dbname = ""; // название Вашей MySQL-базы данных, в которой хранится таблица с информацией о доставках
-	$mysql_tablename = ""; // название MySQL-таблицы, в кторой харнится информация о доставках
+	require_once 'config/config.php';
 
-	$link = mysql_connect($mysql_host, $mysql_user, $mysql_password) 
-        or die('Не удалось соединиться: ' . mysql_error());
-    mysql_select_db($mysql_dbname) or die('Не удалось выбрать базу данных: ' . mysql_error());
-    mysql_set_charset('utf8');
+    $link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_dbname);
+	if (!$link) {
+	    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+	    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+	    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+	    exit;
+	}
+
+	mysqli_set_charset($link, "utf8");
 
     $deliveryCourier = $_GET["deliveryCourier"];
     
-    $result = mysql_query("SELECT * FROM $mysql_tablename WHERE deliveryStatus = '0' AND deliveryCourier = '".mysql_real_escape_string($deliveryCourier)."'");
+    $result = mysqli_query($link, "SELECT * FROM $mysql_tablename WHERE deliveryStatus = '0' AND deliveryCourier = '".mysqli_real_escape_string($link, $deliveryCourier)."'");
 	$rows = array();
-	while($r = mysql_fetch_assoc($result)) {
+	while($r = mysqli_fetch_assoc($result)) {
     	$rows[] = $r;
 	}
 
 	header('Content-Type:Application/json; charset=utf-8');
 	echo json_encode($rows, JSON_UNESCAPED_UNICODE);
 
-	mysql_close($link);
+	//mysqli_free_result($result);
+	mysqli_close($link);
 ?>
