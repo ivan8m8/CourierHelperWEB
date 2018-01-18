714be5ba07f84cb1d407c59c6a3169a82a875033
Ivan <ivan8m8@gmail.com>
2018-01-18 23:16:00 +0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$mysql_host              = "localhost"; // адрес MySQL-сервера
-	$mysql_user              = "courierhelper_test_user"; // имя пользователя для аторизации на Вашем MySQL-сервере
-	$mysql_password          = ""; // пароль для пользовтеля, указанного выше
-	$mysql_dbname            = "courierhelper_test"; // название Вашей MySQL-базы данных, в которой хранится таблица с информацией о доставках
-	$mysql_tablename         = "deliveries"; // название MySQL-таблицы, в кторой харнится информация о доставках
-	$mysql_courierstablename = "couriers"; // название MySQL-таблицы, в кторой харнится информация о курьерах
-?>
