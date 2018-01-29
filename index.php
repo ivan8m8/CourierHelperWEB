65bc4fc543f1c87d208d9babbb4c2c64a34ef461
Ivan <ivan8m8@gmail.com>
2018-01-29 17:34:28 +0300

    Add files via upload
    убрал счётчик

--- index.php
+++ index.php
@@ -976,34 +976,7 @@ if (isset($_POST["send_to_couriers"])) {
     <!-- Site footer -->
       <footer class="footer">
         <p>CourierHelper.ru by Ivan Makachka &copy; 2017</p>
-        <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter47398678 = new Ya.Metrika2({
-                    id:47398678,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true
-                });
-            } catch(e) { }
-        });
-
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/tag.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks2");
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/47398678" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
+ 
       </footer>
 
     </div></center> <!-- /container -->
